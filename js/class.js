44ec1f2489c11023ca862cf9ca2e266a82308c23
TihomirPet <tihomirpetrov@web.de>
2023-07-25 20:56:46 +0200

    add new image profile

--- js/class.js
+++ js/class.js
@@ -3,9 +3,32 @@
 // console.log(a );
 
 
-function el(css) {
-  return document.querySelectorAll(css);
-}
+function el(css){
+	return document.querySelector(css);
+};
+let comment = document.getElementsByClassName('img-hover');
 
-export const link = el('.test');
-console.log(link);
+
+// export const link = el('.test');
+// console.log(link);
+// let dataFromA = localStorage.getItem('myData');
+
+
+// if (dataFromA && dataFromA === '.carousel-item') {
+// console.log("dataFromA");
+
+// let elementToChange = document.getElementById("one")
+// elementToChange.classList.add('test')
+
+// }
+
+ function sendClassNameToB() {
+   let classNameToSend = 'active'; // Replace "desiredClassName" with the class name you want to pass
+   localStorage.setItem('className', classNameToSend);
+ }
+// el('#send').addEventListener('onclick', sendClassNameToB);
+
+
+for (var i = 0; i < comment.length; i++) {
+  comment[i].addEventListener('click', sendClassNameToB);
+}
--- js/classId.js
+++ js/classId.js
@@ -3,8 +3,42 @@ function el(css){
 	return document.querySelectorAll(css);
 };
 
-export const a = el('.test');
-console.log(a);
+
+
+// console.log(dataToPass);
+
+  // Function to read URL parameter
+//   function getParameterByName(name) {
+//     const url = new URL(window.location.href);
+// 	//  console.log(url);
+//     return url.searchParams.get(name);
+//   }
+
+  // Function to change the class name
+//   function changeClassName() {
+//     const className = getParameterByName('classname');
+  
+//     console.log(className);
+//     if (className) {
+//       const targetDiv = el('.test');
+// 	  console.log(targetDiv);
+//       targetDiv.classList.add(className);
+//     }
+//   }
+
+  // Call the function to change the class name when the page loads
+//   window.addEventListener('load', changeClassName);
 
 
 
+  function changeCarouselClassName() {
+    var className = localStorage.getItem('className');
+
+    if (className) {
+      var carouselDiv = document.querySelector('.carousel .carousel-item');
+      carouselDiv.classList.add(className);
+    }
+  }
+
+  // Add event listener to trigger class change on load
+  window.addEventListener('load', changeCarouselClassName);
