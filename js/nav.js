f3589819b2281428e35f0c93ba8dadba24aaf66e
TihomirPet <tihomirpetrov@web.de>
2023-07-26 20:10:27 +0200

    include nav from separate html file

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+function el(css) {
+  return document.querySelector(css);
+}
+
+let nav = el('.nav-holder');
+// const nav = document.querySelector('.nav-holder');
+
+fetch('./navbar.html')
+  .then((res) => res.text())
+  .then((data) => {
+  nav.innerHTML=data
+    //   nav.insertAdjacentHTML('beforeend', data);
+  });
+console.log(nav);
