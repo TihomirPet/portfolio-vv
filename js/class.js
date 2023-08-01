f26a3ee1e04a072aac913d7e9e599c4cb5e2e605
TihomirPet <tihomirpetrov@web.de>
2023-08-01 19:07:04 +0200

    add  js data onclick and  target div on carousel

--- js/class.js
+++ js/class.js
@@ -4,18 +4,39 @@
 function el(css){
 	return document.querySelector(css);
 };
-let comment = document.getElementsByClassName('img-hover');
+// let comment = document.getElementsByClassName('img-hover');
 
 
 
 
- function sendClassNameToB() {
-   let classNameToSend = 'active'; // Replace "desiredClassName" with the class name you want to pass
-   localStorage.setItem('className', classNameToSend);
+//  function sendClassNameToB() {
+//    let classNameToSend = 'active'; // Replace "desiredClassName" with the class name you want to pass
+//    localStorage.setItem('className', classNameToSend);
+//  }
+// // el('#send').addEventListener('onclick', sendClassNameToB);
+
+
+// for (var i = 0; i < comment.length; i++) {
+//   comment[i].addEventListener('click', sendClassNameToB);
+// }
+
+
+
+ document.getElementById('button1').addEventListener('click', function () {
+   sendDataToB('div1', 'active');
+ });
+
+ // Event listener function for Button 2
+ document.getElementById('button2').addEventListener('click', function () {
+   sendDataToB('div2', 'active');
+ });
+
+ // Function to handle sending data to b.html using Local Storage
+ function sendDataToB(targetId, newClassName) {
+   let dataToSend = {
+     id: targetId,
+     className: newClassName,
+   };
+   localStorage.setItem('data', JSON.stringify(dataToSend));
+   window.location.href = 'project-one.html'; // Navigate to b.html after setting the data in Local Storage
  }
-// el('#send').addEventListener('onclick', sendClassNameToB);
-
-
-for (var i = 0; i < comment.length; i++) {
-  comment[i].addEventListener('click', sendClassNameToB);
-}
--- js/classId.js
+++ js/classId.js
@@ -1,44 +1,28 @@
+function el(css) {
+  return document.querySelectorAll(css);
+}
 
-function el(css){
-	return document.querySelectorAll(css);
-};
+// function changeCarouselClassName() {
+//   var className = localStorage.getItem('className');
 
-
-
-// console.log(dataToPass);
-
-  // Function to read URL parameter
-//   function getParameterByName(name) {
-//     const url = new URL(window.location.href);
-// 	//  console.log(url);
-//     return url.searchParams.get(name);
+//   if (className) {
+//     var carouselDiv = document.querySelector('.carousel .carousel-item');
+//     carouselDiv.classList.add(className);
 //   }
+// }
 
-  // Function to change the class name
-//   function changeClassName() {
-//     const className = getParameterByName('classname');
-  
-//     console.log(className);
-//     if (className) {
-//       const targetDiv = el('.test');
-// 	  console.log(targetDiv);
-//       targetDiv.classList.add(className);
-//     }
-//   }
+// // Add event listener to trigger class change on load
+// window.addEventListener('load', changeCarouselClassName);
 
-  // Call the function to change the class name when the page loads
-//   window.addEventListener('load', changeClassName);
+function changeCarouselClassName() {
+  let data = localStorage.getItem('data');
 
-
-
-  function changeCarouselClassName() {
-    var className = localStorage.getItem('className');
-
-    if (className) {
-      var carouselDiv = document.querySelector('.carousel .carousel-item');
-      carouselDiv.classList.add(className);
+  if (data) {
+    const parsedData = JSON.parse(data);
+    const targetDiv = document.getElementById(parsedData.id);
+    if (targetDiv) {
+      targetDiv.classList.add(parsedData.className);
     }
   }
-
-  // Add event listener to trigger class change on load
-  window.addEventListener('load', changeCarouselClassName);
+}
+window.addEventListener('load', changeCarouselClassName);
