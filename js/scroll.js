6215da61e201ab079ee02547aa7901b7e40c3151
TihomirPet <tihomirpetrov@web.de>
2024-09-12 20:34:02 +0200

    add animation header

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother, timeline, SplitText);
   // gsap code here!
   let smoother = ScrollSmoother.create({
     wrapper: '#smooth-wrapper',
@@ -8,20 +8,64 @@ document.addEventListener('DOMContentLoaded', (event) => {
     smoothTouch: 0.1, // Better not to use it or use a small value
     effects: true, // add some effects \(-_-)/
     // some callbacks
-
   });
 });
 
 
+let tl = gsap.timeline();
 
 
 
-// gsap.to('.header-TitleTop', {
-//   x: 700,
+// gsap.from('.header', {
+//   x: 300,
 //   duration: 3,
 //   scrollTrigger: {
-//     trigger: '.header-TitleTop',
+//     trigger: '.header',
 //     start: 'top center',
 //     markers: true,
+   
 //   },
 // });
+
+
+// animation background
+tl.fromTo(
+  '.header-video',
+  { opacity: 0, z: '-800', scale: 2 },
+  {
+    opacity: 1,
+    duration: 2,
+    // stagger: 0.5,
+    scale: 1,
+    ease: 'Power.easeInOut',
+    y: '0',
+  }
+);
+
+
+// animation 
+tl.fromTo(
+  [, '.animateSkils', '.nav-holder', ],
+  { opacity: 0, y: '-50' },
+  {
+    opacity: 1,
+    duration: 0.9,
+    stagger: 0.5,
+    ease: 'Power.easeInOut',
+    y: '0',
+  }
+);
+
+
+
+// (split = new SplitText('header-text', { type: 'words' })),
+//   tl.from(split.chars, {
+//     opacity: 0,
+//     y: 50,
+//     duration: 2,
+//     ease: 'back',
+//     stagger: 0.05,
+//   });
+
+
+
